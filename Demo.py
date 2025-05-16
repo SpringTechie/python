051a0f347d659dbcab8ad6c25e735272b5ef2eda
springtechie <springframeworkdeveloper@gmail.com>
2025-05-16 07:09:46 +0530

    first commit

---
+++ Demo.py
@@ -0,0 +1,25 @@
+# for x in range(1,100):
+#     if(x%2==0):
+#         print(x)
+
+
+even_number_count = 0
+x = 0
+while(even_number_count<=100):
+    if(x%2==0) : 
+        print(x)
+        even_number_count +=1
+    x=x+1 # 3
+
+
+
+
+
+
+
+
+
+
+
+
+        
